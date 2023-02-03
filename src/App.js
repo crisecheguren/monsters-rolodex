04e9bee644c0436f786e2e1efa97039893333060
Cris <crisecheguren@gmail.com>
2023-02-03 14:32:27 -0500

    added app-title and finishing touches

--- src/App.js
+++ src/App.js
@@ -47,6 +47,7 @@ render() {
 
     return (
       <div className="App">
+        <h1 className='app-title'>Monsters Rolodex</h1>
         <SearchBox className='search-box' placeholder='search monsters' onChangeHandler={onSearchChange}/>
         <CardList monsters={filteredMonsters}/>
       </div>
