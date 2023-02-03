93406a46a05ef5971dea0471a8ed51d173f6447d
Cris <crisecheguren@gmail.com>
2023-02-03 11:01:21 -0500

    fleshed out card-list.component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
 
 import './App.css';
 
@@ -46,13 +47,7 @@ render() {
     return (
       <div className="App">
         <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange} />
-        {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
+        <CardList monsters={filteredMonsters}/>
       </div>
     
       
